5896e6c525b174dcc723e7fcbe6a71c613b13e09
Quentin Carrel <dvquentincarrel@gmail.com>
2025-06-01 16:36:31 +0200

    [MA] Move cli "ui" outside of main file, add edit option

---
+++ cli.py
@@ -0,0 +1,66 @@
+#!/bin/python
+import os
+import sys
+os.chdir(os.readlink(sys.argv[0]).rpartition('/')[0])
+
+import atexit
+from main import RecordTable, sec_to_hms
+from config import config
+from time_util import Moment
+from orm import Database
+
+now = Moment.now()
+
+if __name__ == '__main__':
+    db = Database(config['database'], RecordTable)
+    table: "RecordTable" = db.tables['records']
+    atexit.register(lambda: db.connection.close())
+
+    ans = input('\n'.join([
+        "Choose an option by its number:",
+        "  1 - Add entry",
+        "  2 - Add entry before",
+        "  3 - Edit entries",
+        "  4 - See daily entries",
+        "  5 - See weekly entries",
+        "  6 - See monthly entries",
+        "  7 - See yearly entries",
+        "  8 - See all entries",
+        "  9 - See time per entry today",
+        "  99 - debug",
+        "",
+    ]))
+    match ans:
+        case '1':
+            table.add_entry()
+            exit()
+        case '2':
+            ans = input("Negative minutes offset: ")
+            moment = now._offset('minute', -float(ans))
+            table.add_entry(moment=moment)
+            exit()
+        case '3':
+            table.edit()
+            exit()
+        case '4':
+            entries = table.get_current('day')
+        case '5':
+            entries = table.get_current('week')
+        case '6':
+            entries = table.get_current('month')
+        case '7':
+            entries = table.get_current('year')
+        case '8':
+            entries = table.compute_length()
+        case '9':
+            data = {key:sum(row[-1] for row in rows) for key,rows in table.group_by_activity(span='day', with_length=True).items()}
+            for k,v in data.items():
+                print(f"{k}: {sec_to_hms(v)}")
+            exit(0)
+        case '99':
+            breakpoint()
+            exit(0)
+        case _:
+            exit(1)
+    for (time, act, comm, *other) in entries:
+        print(f"{time}: ({act}) ⇒ {comm} {other}")
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/bin/python
-import atexit
 from collections import defaultdict
 from orm import Database, Table
 from config import config
@@ -14,12 +12,12 @@ def sec_to_hms(seconds: int) -> str:
         hours = minutes = seconds = 0
     return f"{hours:02}:{minutes:02}:{seconds:02}"
 
-now = Moment.now()
 class RecordTable(Table):
 
     def _post_refresh(self):
         # Order by time
-        self.values.sort(key=lambda x: x[0])
+        #self.values.sort(key=lambda x: x[0])
+        pass
 
     def add_entry(
         self,
@@ -39,10 +37,10 @@ class RecordTable(Table):
         self.flush()
         # self.parent['activity'].refresh_vals()
 
-    def filter_by_date(self, moment: Moment, span: Span, values=None) -> list[list]:
+    def filter_by_date(self, moment: Moment, span: Span, values: list[tuple] | None = None) -> list[list]:
         """Returns entries encompassing the given span around the given moment"""
         if values is None:
-            values = self.values
+            values = tuple(self.values.values())
 
         start, end = map(lambda x: str(x), moment.range(span))
         return list(filter(lambda val: start <= val[0] < end , values))
@@ -50,7 +48,7 @@ class RecordTable(Table):
 
     def get_current(self, span: Span) -> list[list]:
         """Returns current entries encompassing the given span"""
-        return self.filter_by_date(now, span)
+        return self.filter_by_date(Moment.now(), span)
 
     def group_by_activity(self, span=None, moment=None, with_length=False, exclude_stop=True):
         """Regroups activity filtered around a span for a given moment by
@@ -60,9 +58,9 @@ class RecordTable(Table):
         :param moment: Moment around which to apply the timespan. Now if None
         :param with_length: Compute length of the rows beforehand
         """
-        values = self.compute_length() if with_length else self.values
+        values = self.compute_length() if with_length else tuple(self.values.values())
         if moment is None:
-            moment = now
+            moment = Moment.now()
         entries = self.filter_by_date(moment, span, values) if span else values
 
         rows_map = defaultdict(list)
@@ -82,7 +80,8 @@ class RecordTable(Table):
         """
         entries = []
         next_moment = Moment.now()
-        for entry in self.values[::-1]:
+        values = sorted(list(self.values.values()), key=lambda x: x[0])
+        for entry in values[::-1]:
             cur_moment = Moment.from_row(entry[0])
             if entry[1] == 'stop':
                 next_moment = cur_moment
@@ -92,53 +91,3 @@ class RecordTable(Table):
 
         entries.reverse()
         return entries
-
-
-if __name__ == '__main__':
-    atexit.register(lambda: db.connection.close())
-    db = Database(config['database'], RecordTable)
-    table: "RecordTable" = db.tables['records']
-    ans = input('\n'.join([
-        "Choose an option by its number:",
-        "  1 - Add entry",
-        "  2 - Add entry before",
-        "  3 - See daily entries",
-        "  4 - See weekly entries",
-        "  5 - See monthly entries",
-        "  6 - See yearly entries",
-        "  7 - See all entries",
-        "  8 - See time per entry today",
-        "  99 - debug",
-        "",
-    ]))
-    match ans:
-        case '1':
-            table.add_entry()
-            exit()
-        case '2':
-            ans = input("Negative minutes offset: ")
-            moment = now._offset('minute', -float(ans))
-            table.add_entry(moment=moment)
-            exit()
-        case '3':
-            entries = table.get_current('day')
-        case '4':
-            entries = table.get_current('week')
-        case '5':
-            entries = table.get_current('month')
-        case '6':
-            entries = table.get_current('year')
-        case '7':
-            entries = table.compute_length()
-        case '8':
-            data = {key:sum(row[-1] for row in rows) for key,rows in table.group_by_activity(span='day', with_length=True).items()}
-            for k,v in data.items():
-                print(f"{k}: {sec_to_hms(v)}")
-            exit(0)
-        case '99':
-            breakpoint()
-            exit(0)
-        case _:
-            exit(1)
-    for (time, act, comm, *other) in entries:
-        print(f"{time}: ({act}) ⇒ {comm} {other}")
